4f7092f67f513e6e44c0cc964a2b287b07f1e0eb
deepak21devops <71614832+deepak21devops@users.noreply.github.com>
2021-09-05 14:58:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './componenets/css/style.css'
+import './App.css';
+import SampleData from './componenets/SampleData';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SampleData />
+      </header>
+    </div>
+  );
+}
+
+export default App;
